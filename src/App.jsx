8439a4d24e00c56b3507bfc595e67cec1bae7828
yangbrulapha-tech <yangbrulapha@gmail.com>
2026-06-29 12:04:11 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import { supabase } from './supabaseClient'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
